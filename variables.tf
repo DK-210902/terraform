40069f66a4cc08264ea27bdf58f64ab57f47cd05
DK-210902 <156884947+DK-210902@users.noreply.github.com>
2024-09-11 10:58:59 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,10 @@
-variable "akey" {
+variable "AWS_ACCESS_KEY_ID" {
   type = string
 }
-variable "skey" {
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
+variable "AWS_SESSION_TOKEN" {
   type = string
 }
 variable "location" {
